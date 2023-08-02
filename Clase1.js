cc79c0573524ae70c02c3693bc63ffbd2a0d7c48
Monalisa330 <laurameliza26@gmail.com>
2023-08-01 20:06:53 -0500

    first commit

---
+++ Clase1.js
@@ -0,0 +1,8 @@
+//Se declaran las variables y se inicializan
+const firstName = "Laura";
+const last_Name = "Álvarez";
+const birthYear = 2001 ;
+const city = "Medellín";
+
+console.log("Hola, mi nombre es",firstName,last_Name,"\n nací en el", birthYear,",tengo 21 años y vivo en",city)
+
